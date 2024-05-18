b8acd2580866c930b57ef8769b8b78ea84ad04a6
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-05-18 17:10:26 +0500

    Add files via upload

---
+++ task22.js
@@ -0,0 +1,5 @@
+"use strict";
+// task 22 intentional error: try to make array index error happen.
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "saturday", "Sunday"];
+// console.log(days{6})
+console.log(days[6]);
---
+++ task22.ts
@@ -0,0 +1,4 @@
+// task 22 intentional error: try to make array index error happen.
+const days:string[] = ["Monday","Tuesday","Wednesday","Thursday","Friday","saturday","Sunday"]
+// console.log(days{6})
+console.log(days[6])
